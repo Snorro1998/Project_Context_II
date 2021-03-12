5067ab039ccf86f88da227f4a6f56e3e44583c8d
PepijnvanBergen <pepijn0328@gmail.com>
2021-03-12 15:26:10 +0100

    Nieuwe scenes toegevoegd

--- Assets/Scripts/PepijnScirpts/GameManager.cs
+++ Assets/Scripts/PepijnScirpts/GameManager.cs
@@ -8,10 +8,30 @@ public class GameManager : MonoBehaviour
     public static bool boatIsMoving;
     public static bool canBoatmove = true;
     public static bool isInCutscene;
+    int counter;
     public static Vector3 boatPosition = new Vector3();
-
-    void Update()
+    public static GameManager Instance
     {
-        
+        get;
+        private set;
+    }
+
+    protected virtual void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+    private void Update()
+    {
+        counter++;
+        counter /= 60;
+        Debug.Log(counter);
     }
 }
